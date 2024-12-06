0dde68a55bf6e9300fd6ab7320ef34b39321ff3d
salmanshahid5 <salmanshahid0333@gmail.com>
2024-12-06 21:33:23 +0500

    models files create

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.send('welcome to youtube')
+})
+
 app.listen(3000,()=>{
     console.log("connected");
     
